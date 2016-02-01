5b1c3012c8db01e152c975c87892962b9c90adcb
EdwinYeh <edwin02621@gmail.com>
2016-02-02 03:02:46 +0800

    ..

--- Exp_ours_allfree_cvx_toy_sigma.m
+++ Exp_ours_allfree_cvx_toy_sigma.m
@@ -1,7 +1,7 @@
 sigmaList = [0.1, 1, 10, 50, 100, 250];
 
 for sigmaTryTime = 1:6
-    datasetId = 1;
+%     datasetId = 1;
     SetParameter;
     numSampleFeature = 2;
     lambdaTryTime = 3;
@@ -9,5 +9,5 @@ for sigmaTryTime = 1:6
     exp_title = sprintf('ours_%d_allfree_cvx_sigma_%f', datasetId, sigma);
     PrepareExperiment2;
     showExperimentInfo(exp_title, datasetId, prefix, numSampleInstance, numSampleFeature, numInstanceCluster, numFeatureCluster, sigma);
-    Main_ours_allfree_cvx;
+    main_ours_allfree_cvx;
 end
