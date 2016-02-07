17314dbe9be03f18e2d974881be1d8de2264c6a9
EdwinYeh <edwin02621@gmail.com>
2016-02-07 17:35:26 +0800

    (1) Fix bug in script

--- script/run_multi_dataset_cvx_sigma.sh
+++ script/run_multi_dataset_cvx_sigma.sh
@@ -2,5 +2,5 @@ Idx_dataSet="1 2 3 4 5 6 7 8 9 10 11 12 13"
 
 for idx in $Idx_dataSet
 do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; ../Exp_ours_allfree_cvx_sigma; exit;" &
+	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; cd..; Exp_ours_allfree_cvx_sigma; exit;" &
 done
--- script/run_multi_dataset_sigma.sh
+++ script/run_multi_dataset_sigma.sh
@@ -2,6 +2,5 @@ Idx_dataSet="1 2 3 4 5 6 7 8 9 10 11 12 13"
 
 for idx in $Idx_dataSet
 do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; ../Exp_ours_sigma; exit;" &
+	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; cd ..; Exp_ours_sigma; exit;" &
 done
-
