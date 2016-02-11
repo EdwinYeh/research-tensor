0b2cd6b76d957b4d79c6cfd1e73744b7afd1353e
EdwinYeh <edwin02621@gmail.com>
2016-02-11 17:38:54 +0800

    (1) Change the way of select hyper-parameter (2) Fix scripts

--- Exp_ours_sigma.m
+++ Exp_ours_sigma.m
@@ -1,8 +1,6 @@
 SetParameter;
+isTestPhase = true;
 exp_title = sprintf('ours_%d_sigma', datasetId);
-bestValidateAccuracy = 0;
-bestLambda = 0;
-bestSigma = 0;
 lambdaTryTime = 3;
 sigmaList = [0.1,  0.5, 1, 10, 100];
 for sigmaTryTime = 1:length(sigmaList)
@@ -12,21 +10,5 @@ for sigmaTryTime = 1:length(sigmaList)
         lambda = 0.00000001 * 100 ^ tuneLambda;
         showExperimentInfo(exp_title, datasetId, prefix, numSampleInstance, numSampleFeature, numInstanceCluster, numFeatureCluster, sigma);
         main_ours;
-        if accuracy > bestValidateAccuracy
-            bestValidateAccuracy = accuracy;
-            bestLambda = lambda;
-            bestSigma = sigma;
-        end
     end
 end
-
-fprintf('Start testing\n');
-SetParameter;
-isTestPhase = true;
-% randomTryTime = 5;
-sigma = bestSigma;
-lambda = bestLambda;
-PrepareExperiment;
-exp_title = sprintf('ours_%d_sigma', datasetId);
-showExperimentInfo(exp_title, datasetId, prefix, numSampleInstance, numSampleFeature, numInstanceCluster, numFeatureCluster, sigma);
-main_ours;
--- script/run_multi_dataset_cvx_sigma.sh
+++ script/run_multi_dataset_cvx_sigma.sh
@@ -1,6 +1,5 @@
-Idx_dataSet="3 5 6 9 10"
-
+Idx_dataset="1 2 3 4 5 6 7 8 9 10 11 12 13"
 for idx in $Idx_dataSet
 do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; cd ..; Exp_ours_allfree_cvx_sigma; exit;" &
+	/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; datasetId=$idx; Exp_ours_allfree_cvx_sigma; exit;" &
 done
