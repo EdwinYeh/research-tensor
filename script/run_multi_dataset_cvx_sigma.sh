6efc2dad3fab3e9f9f00b83389e8e3f43e77377b
EdwinYeh <edwin02621@gmail.com>
2016-02-11 21:41:40 +0800

    (1) Rebuild scripts

--- script/run_multi_dataset_cvx_sigma.sh
+++ script/run_multi_dataset_cvx_sigma.sh
@@ -1,5 +1,6 @@
 Idx_dataset="1 2 3 4 5 6 7 8 9 10 11 12 13"
-for idx in $Idx_dataSet
+
+for idx in $Idx_dataset
 do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; datasetId=$idx; Exp_ours_allfree_cvx_sigma; exit;" &
+	/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; datasetId = $idx; Exp_ours_allfree_cvx_sigma; exit;" &
 done
