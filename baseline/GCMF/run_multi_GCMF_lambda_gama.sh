2f4b081355d6d8a75521bfe2c9bfb2bd02f4343d
EdwinYeh <edwin02621@gmail.com>
2016-03-17 16:03:09 +0800

    ...

--- baseline/GCMF/run_multi_GCMF_lambda_gama.sh
+++ baseline/GCMF/run_multi_GCMF_lambda_gama.sh
@@ -2,5 +2,5 @@ Idx_dataset="1 2 3 4 5 6 7 8 9 10 11 12 13"
 
 for idx in $Idx_dataset
 do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; Exp_GCMF_sigma_gama exit;"
+	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; Exp_GCMF_sigma_gama; exit;"
 done
