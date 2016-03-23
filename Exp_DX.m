2369667542beb32ce81c4f61c73e1c8348d56e0e
EdwinYeh <edwin02621@gmail.com>
2016-03-23 15:47:51 +0800

    Fix bug of random initialize variables

--- Exp_DX.m
+++ Exp_DX.m
@@ -1,23 +1,29 @@
 % Please assign datasetId in the commend line
 
-sigmaList = [0.05, 0.1, 0.4, 0.7, 1.0];
+sigmaList = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 numInstanceClusterList = [5, 10, 20, 40];
 numFeatureClusterList = [5, 10, 20, 40];
 cpRankList = [5, 10, 20, 40];
+expTitle = 'DX';
 
-directoryName = sprintf('../exp_result/DX/%d/', datasetId);
+directoryName = sprintf('../exp_result/%s/%d/', expTitle, datasetId);
 mkdir(directoryName);
 
-for tuneCPRank = 1: length(cpRankList)
-    cpRank = cpRankList(tuneCPRank);
-    for tuneNumFeatureCluster = 1: length(numFeatureClusterList)
-        numFeatureCluster = numFeatureClusterList(tuneNumFeatureCluster);
-        for tuneNumInstanceCluster = 1: length(numInstanceClusterList)
-            numInstanceCluster = numInstanceClusterList(tuneNumInstanceCluster);
-            for tuneSigma = 1: length(sigmaList)
-                sigma = sigmaList(tuneSigma);
-                prepareDXExperiment;
-                main_DX_new_project;
+for tuneSigma = 1: length(sigmaList)
+    sigma = sigmaList(tuneSigma);
+    for tuneCPRank = 1: length(cpRankList)
+        cpRank = cpRankList(tuneCPRank);
+        for tuneNumFeatureCluster = 1: length(numFeatureClusterList)
+            numFeatureCluster = numFeatureClusterList(tuneNumFeatureCluster);
+            for tuneNumInstanceCluster = 1: length(numInstanceClusterList)
+                numInstanceCluster = numInstanceClusterList(tuneNumInstanceCluster);
+                try
+                    prepareDXExperiment;
+                    main_DX;
+                catch exception
+                    fprintf('%g,%g,%g,%d,%d,%d\n', lambda, gama, sigma, numInstanceCluster, numFeatureCluster, cpRank);
+                    disp(exception.message);
+                end
             end
         end
     end
