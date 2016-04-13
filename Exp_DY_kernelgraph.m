f0cf6fca35a784425c878ca7501806b421a63d17
EdwinYeh <edwin02621@gmail.com>
2016-04-13 22:51:51 +0800

    (1) Prepared for new one norm and kernel graph exp (2) Delete unnessesary files

--- Exp_DY_kernelgraph.m
+++ Exp_DY_kernelgraph.m
@@ -1,18 +1,18 @@
 SetParameter;
 isTestPhase = true;
 exp_title = sprintf('DY_kernelgraph_%d', datasetId);
-lambdaTryTime = 5;
-sigmaTryTime = 10;
-sigma2TryTime = 10;
-randomTryTime = 2;
-delta = -1;
-for sigmaOrder = 1:sigmaTryTime
-    sigma = (10^-5) * (10^sigmaOrder);
-    for sigma2Order = 1:sigma2TryTime
-        sigma2 = (10^-5) * (10^sigma2Order);
-        prepareExperimentKernelGraph;
+lambdaMaxOrder = 10;
+sigmaMaxOrder = 15;
+sigma2MaxOrder = 15;
+randomTryTime = 5;
+delta = 0;
+for sigmaOrder = 1:sigmaMaxOrder
+    sigma = (1) * (10^sigmaOrder);
+    for sigma2Order = 1:sigma2MaxOrder
+        sigma2 = (1) * (10^sigma2Order);
+        PrepareExperimentKernelGraph;
         for tuneLambda = 0:lambdaTryTime
-            lambda = 0.00000001 * 100 ^ tuneLambda;
+            lambda = 0.00000001 * 10 ^ tuneLambda;
             main_DY_newproj;
         end
     end
--- script/run_multi_dataset_instance_cluster.sh
+++
@@ -1,6 +0,0 @@
-Idx_dataSet="1 2 3 4 5 6 7 8 9 10"
-
-for idx in $Idx_dataSet
-do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "datasetId=$idx; cd../; Exp_ours_num_instance_cluster; exit;" &
-done
