e151da2502c99808d0b117bb54d27ed5dfb9d7be
EdwinYeh <edwin02621@gmail.com>
2016-04-13 22:57:15 +0800

    ...

--- Exp_DY_onenorm.m
+++ Exp_DY_onenorm.m
@@ -13,9 +13,9 @@ for sigmaTryTime = 1:length(sigmaList)
     lambda = 0.00000001 * 10 ^ lambdaOrder;
         for deltaOrder = 0:deltaMaxOrder
           delta = 0.000000001 * 10 ^ deltaOrder;
-          main_DY_newproj;
+          main_DY_onenorm;
         end
         delta = 0;
-        main_DY_newproj;
+        main_DY_onenorm;
     end
 end
