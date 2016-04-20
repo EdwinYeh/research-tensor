503cc956b5a4843cd035ce9dba9d2da428fd3962
EdwinYeh <edwin02621@gmail.com>
2016-04-21 01:35:45 +0800

    [correct version] Fix bug of fake optimization

--- mylib/ShowObjective.m
+++ mylib/ShowObjective.m
@@ -1,15 +1,15 @@
-function tmpObjectiveScore = ShowObjective(U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda)
+function tmpObjectiveScore = ShowObjective(fold, U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda)
     tmpObjectiveScore = 0;
-    for i = 1:2
-        [A,sumFi,E] = projectTensorToMatrix({CP1,CP2,CP3,CP4}, i);
+    for dom = 1:2
+        [A,sumFi,E] = projectTensorToMatrix({CP1{fold},CP2{fold},CP3{fold},CP4{fold}}, dom);
         projB = A*sumFi*E';
-        result = U{i}*projB*V{i}';
-        if i == 2
-            normEmp = norm((YMatrix{i} - result).*W, 'fro')*norm((YMatrix{i} - result).*W, 'fro');
+        result = U{fold, dom}*projB*V{fold, dom}';
+        if dom == 2
+            normEmp = norm((YMatrix{dom} - result).*W, 'fro')*norm((YMatrix{dom} - result).*W, 'fro');
         else
-            normEmp = norm((YMatrix{i} - result), 'fro')*norm((YMatrix{i} - result), 'fro');
+            normEmp = norm((YMatrix{dom} - result), 'fro')*norm((YMatrix{dom} - result), 'fro');
         end
-        smoothU = lambda*trace(U{i}'*Lu{i}*U{i});
+        smoothU = lambda*trace(U{fold, dom}'*Lu{dom}*U{fold, dom});
         objectiveScore = normEmp + smoothU;
         tmpObjectiveScore = tmpObjectiveScore + objectiveScore;
     end
