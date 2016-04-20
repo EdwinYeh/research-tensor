e9bb4adb67d819c7d8ffc82f0414557031f4ad63
EdwinYeh <edwin02621@gmail.com>
2016-04-21 03:36:26 +0800

    Rewrite sh of DY_onenorm

--- script/run_multi_DY_onenorm.sh
+++ script/run_multi_DY_onenorm.sh
@@ -1,6 +1,6 @@
 Idx_dataset="1 2 3 4 5 6 7 8 9 10 11 12 13"
 
-for Idx in $Idx_dataset
+for idx in $Idx_dataset
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd..; datasetId = $Idx; Exp_DY_onenorm; exit;" &
+    /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; datasetId = $idx; Exp_DY_onenorm; exit;" &
 done
