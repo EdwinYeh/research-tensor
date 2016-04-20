28f488a06652ab27c4fa086485c7f90fb565a9ae
EdwinYeh <edwin02621@gmail.com>
2016-04-20 23:32:57 +0800

    Fix bug of fake optimization

--- Exp_DY_onenorm.m
+++ Exp_DY_onenorm.m
@@ -3,10 +3,10 @@ isTestPhase = true;
 exp_title = sprintf('DY_onenorm_%d', datasetId);
 resultFile = fopen(sprintf('../exp_result/%s.csv', exp_title), 'a');
 fprintf(resultFile, 'sigma,sigma2,lambda,delta,objectiveScore,accuracy,trainingTime\n');
-lambdaMaxOrder = 12;
-deltaMaxOrder = 12;
-randomTryTime = 10;
-sigmaList = 0.1:0.05:0.5;
+lambdaMaxOrder = 0;
+deltaMaxOrder = 0;
+randomTryTime = 1;
+sigmaList = 0.1:0.05:0.1;
 sigma2 = -1;
 for tuneSigma = 1:length(sigmaList)
     sigma = sigmaList(tuneSigma);
--- main_DY_onenorm.m
+++ main_DY_onenorm.m
@@ -7,153 +7,154 @@ bestAccuracy = 0;
 bestTime = 0;
 SU=cell(1,2);
 SV=cell(1,2);
-U = cell(2,1);
-V = cell(2,1);
-realU = cell(2,1);
-realV = cell(2,1);
+U = cell(randomTryTime,numCVFold,2,1);
+V = cell(randomTryTime,numCVFold,2,1);
+realU = cell(randomTryTime,numCVFold,2,1);
+realV = cell(randomTryTime,numCVFold,2,1);
+CP1 = cell(randomTryTime,numCVFold);
+CP2 = cell(randomTryTime,numCVFold);
+CP3 = cell(randomTryTime,numCVFold);
+CP4 = cell(randomTryTime,numCVFold);
+realCP1 = cell(randomTryTime,numCVFold);
+realCP2 = cell(randomTryTime,numCVFold);
+realCP3 = cell(randomTryTime,numCVFold);
+realCP4 = cell(randomTryTime,numCVFold);
 
 for t = 1: randomTryTime
-
-    CP1 = rand(numInstanceCluster, cpRank);
-    CP2 = rand(numFeatureCluster, cpRank);
-    CP3 = rand(numInstanceCluster, cpRank);
-    CP4 = rand(numFeatureCluster, cpRank);
-    
-    for dom = 1:2
-        U{dom} = rand(numSampleInstance(dom), numInstanceCluster);
-        V{dom} = rand(2, numFeatureCluster);
+    for fold = 1: numCVFold
+        CP1{t, fold} = rand(numInstanceCluster, cpRank);
+        CP2{t, fold} = rand(numFeatureCluster, cpRank);
+        CP3{t, fold} = rand(numInstanceCluster, cpRank);
+        CP4{t, fold} = rand(numFeatureCluster, cpRank);
+        
+        for dom = 1: 2
+            U{t, fold, dom} = rand(numSampleInstance(dom), numInstanceCluster);
+            V{t, fold, dom} = rand(2, numFeatureCluster);
+        end
     end
+end
 
-    realCP1 = rand(numInstanceCluster, cpRank);
-    realCP2 = rand(numFeatureCluster, cpRank);
-    realCP3 = rand(numInstanceCluster, cpRank);
-    realCP4 = rand(numFeatureCluster, cpRank);
-
-    for dom = 1:2
-        realU{dom} = rand(numSampleInstance(dom), numInstanceCluster);
-        realV{dom} = rand(2, numFeatureCluster);
-    end
-    
+for t = 1: randomTryTime
     % When fakeOptimization == 1, train UVAE  to be the initial points
     % during fakeOptimization == 2. Only the report the result of
     % fakeOptimization == 2 will be report
     
-    for fakeOptimization = 1:2
+    for fakeOptimization = 1: 3
         numCorrectPredict = 0;
         validateIndex = 1: CVFoldSize;
         TotalTimer = tic;
         foldObjectiveScores = zeros(1,numCVFold);
         
-        if fakeOptimization == 2
-            CP1 = realCP1;
-            CP2 = realCP2;
-            CP3 = realCP3;
-            CP4 = realCP4;
-            U = realU;
-            V = realV;
-        end
-
         for fold = 1:numCVFold
             YMatrix = TrueYMatrix;
             YMatrix{targetDomain}(validateIndex, :) = zeros(CVFoldSize, numClass(1));
             W = ones(numSampleInstance(targetDomain), numClass(1));
             W(validateIndex, :) = 0;
             [rY,cY]=size(YMatrix{1});
-            SU{1}=eye(rY);
-            SU{2}=SU{1};
-            SV{1}=eye(cY);
-            SV{2}=SV{1};
+            SU{1} = eye(rY);
+            SU{2} = SU{1};
+            SV{1} = eye(cY);
+            SV{2} = SV{1};
             iter = 0;
             diff = Inf;
             newObjectiveScore = Inf;
-
+            
+            if fakeOptimization == 3
+                CP1{t, fold} = realCP1{t, fold};
+                CP2{t, fold} = realCP2{t, fold};
+                CP3{t, fold} = realCP3{t, fold};
+                CP4{t, fold} = realCP4{t, fold};
+                U{t, fold} = realU{t, fold};
+                V{t, fold} = realV{t, fold};
+            end
+            
             while (diff >= 0.001  && iter < maxIter)
                 iter = iter + 1;
-                % fprintf('Fold:%d,Iteration:%d, ObjectiveScore:%g\n', fold, iter, newObjectiveScore);
+                fprintf('Fake:%d, Fold:%d,Iteration:%d, ObjectiveScore:%g\n', fakeOptimization, fold, iter, newObjectiveScore);
                 oldObjectiveScore = newObjectiveScore;
                 tmpOldObj=oldObjectiveScore;
                 for dom = 1:numDom
-                    [A,sumFi,E] = projectTensorToMatrix({CP1,CP2,CP3,CP4}, dom);
+                    [A,sumFi,E] = projectTensorToMatrix({CP1{t,fold},CP2{t,fold},CP3{t,fold},CP4{t,fold}}, dom);
                     projB = A*sumFi*E';
-
+                    
                     if dom == targetDomain
-                        V{dom} = V{dom}.*sqrt(((YMatrix{dom}.*W)'*U{dom}*projB)./(V{dom}*V{dom}'*(V{dom}*projB'*U{dom}'.*W')*U{dom}*projB));
+                        V{t,fold,dom} = V{t,fold,dom}.*sqrt(((YMatrix{dom}.*W)'*U{t,fold,dom}*projB)./(V{t,fold,dom}*V{t,fold,dom}'*(V{t,fold,dom}*projB'*U{t,fold,dom}'.*W')*U{t,fold,dom}*projB));
                     else
-                        V{dom} = V{dom}.*sqrt((YMatrix{dom}'*U{dom}*projB)./(V{dom}*V{dom}'*(V{dom}*projB'*U{dom}')*U{dom}*projB));
-                    end                    
-                    V{dom}(isnan(V{dom})) = 0;
-                    V{dom}(~isfinite(V{dom})) = 0;                    
-%                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda);
-%                     if tmpObjectiveScore > tmpOldObj
-%                         fprintf('Domain:%d, Objective increased when update V (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
-%                     end
-%                     tmpOldObj=tmpObjectiveScore;
+                        V{t,fold,dom} = V{t,fold,dom}.*sqrt((YMatrix{dom}'*U{t,fold,dom}*projB)./(V{t,fold,dom}*V{t,fold,dom}'*(V{t,fold,dom}*projB'*U{t,fold,dom}')*U{t,fold,dom}*projB));
+                    end
+                    V{t,fold,dom}(isnan(V{t,fold,dom})) = 0;
+                    V{t,fold,dom}(~isfinite(V{t,fold,dom})) = 0;
+                    %                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1{t,fold}, CP2{t,fold}, CP3{t,fold}, CP4{t,fold}, lambda);
+                    %                     if tmpObjectiveScore > tmpOldObj
+                    %                         fprintf('Domain:%d, Objective increased when update V (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
+                    %                     end
+                    %                     tmpOldObj=tmpObjectiveScore;
                     %update U
                     if dom == targetDomain
-                        U{dom} = U{dom}.*sqrt(((YMatrix{dom}.*W)*V{dom}*projB'+lambda*Su{dom}*U{dom})./(U{dom}*U{dom}'*(U{dom}*projB*V{dom}'.*W)*V{dom}*projB'+lambda*Du{dom}*U{dom}));
+                        U{t,fold,dom} = U{t,fold,dom}.*sqrt(((YMatrix{dom}.*W)*V{t,fold,dom}*projB'+lambda*Su{dom}*U{t,fold,dom})./(U{t,fold,dom}*U{t,fold,dom}'*(U{t,fold,dom}*projB*V{t,fold,dom}'.*W)*V{t,fold,dom}*projB'+lambda*Du{dom}*U{t,fold,dom}));
                     else
-                        U{dom} = U{dom}.*sqrt((YMatrix{dom}*V{dom}*projB'+lambda*Su{dom}*U{dom})./(U{dom}*U{dom}'*U{dom}*projB*V{dom}'*V{dom}*projB'+lambda*Du{dom}*U{dom}));
+                        U{t,fold,dom} = U{t,fold,dom}.*sqrt((YMatrix{dom}*V{t,fold,dom}*projB'+lambda*Su{dom}*U{t,fold,dom})./(U{t,fold,dom}*U{t,fold,dom}'*U{t,fold,dom}*projB*V{t,fold,dom}'*V{t,fold,dom}*projB'+lambda*Du{dom}*U{t,fold,dom}));
                     end
-                    U{dom}(isnan(U{dom})) = 0;
-                    U{dom}(~isfinite(U{dom})) = 0;                    
-%                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda);
-%                     if tmpObjectiveScore > tmpOldObj
-%                         fprintf('Domain:%d, Objective increased when update U (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
-%                     end
-%                     tmpOldObj=tmpObjectiveScore;
+                    U{t,fold,dom}(isnan(U{t,fold,dom})) = 0;
+                    U{t,fold,dom}(~isfinite(U{t,fold,dom})) = 0;
+                    %                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1{t,fold}, CP2{t,fold}, CP3{t,fold}, CP4{t,fold}, lambda);
+                    %                     if tmpObjectiveScore > tmpOldObj
+                    %                         fprintf('Domain:%d, Objective increased when update U (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
+                    %                     end
+                    %                     tmpOldObj=tmpObjectiveScore;
                     %update AE
                     if dom == sourceDomain
-                        A = CP1;
-                        E = CP2;
+                        A = CP1{t,fold};
+                        E = CP2{t,fold};
                     else
-                        A = CP3;
-                        E = CP4;
+                        A = CP3{t,fold};
+                        E = CP4{t,fold};
                     end
                     
                     [rA, cA] = size(A);
                     [rE, cE] = size(E);
                     
                     if dom ==targetDomain
-                        A = A.*sqrt((U{dom}'*(YMatrix{dom}.*W)*V{dom}*E*sumFi)./(U{dom}'*(U{dom}*A*sumFi*E'*V{dom}'.*W)*V{dom}*E*sumFi+delta*ones(rE,rA)'*(sumFi*E')'));
+                        A = A.*sqrt((U{t,fold,dom}'*(YMatrix{dom}.*W)*V{t,fold,dom}*E*sumFi)./(U{t,fold,dom}'*(U{t,fold,dom}*A*sumFi*E'*V{t,fold,dom}'.*W)*V{t,fold,dom}*E*sumFi+delta*ones(rE,rA)'*(sumFi*E')'));
                     else
-                        A = A.*sqrt((U{dom}'*YMatrix{dom}*V{dom}*E*sumFi)./(U{dom}'*U{dom}*A*sumFi*E'*V{dom}'*V{dom}*E*sumFi+delta*ones(rE,rA)'*(sumFi*E')'));
+                        A = A.*sqrt((U{t,fold,dom}'*YMatrix{dom}*V{t,fold,dom}*E*sumFi)./(U{t,fold,dom}'*U{t,fold,dom}*A*sumFi*E'*V{t,fold,dom}'*V{t,fold,dom}*E*sumFi+delta*ones(rE,rA)'*(sumFi*E')'));
                     end
                     A(isnan(A)) = 0;
                     A(~isfinite(A)) = 0;
                     if dom == sourceDomain
-                        CP1 = A;
+                        CP1{t,fold} = A;
                     else
-                        CP3 = A;
+                        CP3{t,fold} = A;
                     end
-%                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda);
-%                     if tmpObjectiveScore > tmpOldObj
-%                         fprintf('Domain:%d, Objective increased when update A (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
-%                     end
-%                     tmpOldObj = tmpObjectiveScore;
+                    %                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1{t,fold}, CP2{t,fold}, CP3{t,fold}, CP4{t,fold}, lambda);
+                    %                     if tmpObjectiveScore > tmpOldObj
+                    %                         fprintf('Domain:%d, Objective increased when update A (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
+                    %                     end
+                    %                     tmpOldObj = tmpObjectiveScore;
                     if dom == targetDomain
-                        E = E.*sqrt((V{dom}'*(YMatrix{dom}.*W)'*U{dom}*A*sumFi)./(V{dom}'*(V{dom}*E*sumFi*A'*U{dom}'.*W')*U{dom}*A*sumFi+delta*ones(rE,rA)*A*sumFi));
+                        E = E.*sqrt((V{t,fold,dom}'*(YMatrix{dom}.*W)'*U{t,fold,dom}*A*sumFi)./(V{t,fold,dom}'*(V{t,fold,dom}*E*sumFi*A'*U{t,fold,dom}'.*W')*U{t,fold,dom}*A*sumFi+delta*ones(rE,rA)*A*sumFi));
                     else
-                        E = E.*sqrt((V{dom}'*YMatrix{dom}'*U{dom}*A*sumFi)./(V{dom}'*V{dom}*E*sumFi*A'*U{dom}'*U{dom}*A*sumFi+delta*ones(rE,rA)*A*sumFi));
+                        E = E.*sqrt((V{t,fold,dom}'*YMatrix{dom}'*U{t,fold,dom}*A*sumFi)./(V{t,fold,dom}'*V{t,fold,dom}*E*sumFi*A'*U{t,fold,dom}'*U{t,fold,dom}*A*sumFi+delta*ones(rE,rA)*A*sumFi));
                     end
                     E(isnan(E)) = 0;
                     E(~isfinite(E)) = 0;
                     if dom == sourceDomain
-                        CP2 = E;
+                        CP2{t,fold} = E;
                     else
-                        CP4 = E;
+                        CP4{t,fold} = E;
                     end
-%                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda);
-%                     if tmpObjectiveScore > tmpOldObj
-%                         fprintf('Domain:%d, Objective increased when update E (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
-%                     end
-%                     tmpOldObj=tmpObjectiveScore;
+                    %                     tmpObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1{t,fold}, CP2{t,fold}, CP3{t,fold}, CP4{t,fold}, lambda);
+                    %                     if tmpObjectiveScore > tmpOldObj
+                    %                         fprintf('Domain:%d, Objective increased when update E (%f=>%f)\n', dom, tmpOldObj, tmpObjectiveScore);
+                    %                     end
+                    %                     tmpOldObj=tmpObjectiveScore;
                 end
-                newObjectiveScore = ShowObjectiveS(SU,SV,U, V, W, YMatrix, Lu, CP1, CP2, CP3, CP4, lambda);
+                newObjectiveScore = ShowObjective(U, V, W, YMatrix, Lu, CP1{t,fold}, CP2{t,fold}, CP3{t,fold}, CP4{t,fold}, lambda);
                 diff = oldObjectiveScore - newObjectiveScore;
             end
             foldObjectiveScores(fold) = newObjectiveScore;
             %calculate validationScore
-            [A,sumFi,E] = projectTensorToMatrix({CP1,CP2,CP3,CP4}, targetDomain);
+            [A,sumFi,E] = projectTensorToMatrix({CP1{t,fold},CP2{t,fold},CP3{t,fold},CP4{t,fold}}, targetDomain);
             projB = A*sumFi*E';
             result = U{targetDomain}*projB*V{targetDomain}';
             [~, maxIndex] = max(result, [], 2);
@@ -164,30 +165,30 @@ for t = 1: randomTryTime
                 end
             end
             validateIndex = validateIndex + CVFoldSize;
+            
+            if fakeOptimization == 2
+                realCP1{t, fold} = CP1{t, fold};
+                realCP2{t, fold} = CP2{t, fold};
+                realCP3{t, fold} = CP3{t, fold};
+                realCP4{t, fold} = CP4{t, fold};
+                realU{t, fold} = U{t, fold};
+                realV{t, fold} = V{t, fold};
+            end
         end
-
-        if fakeOptimization == 1
-            realCP1 = CP1;
-            realCP2 = CP2;
-            realCP3 = CP3;
-            realCP4 = CP4;
-            realU = U;
-            realV = V;
-        end
-
-        if fakeOptimization == 2
+        
+        if fakeOptimization == 3
             accuracy = numCorrectPredict/ numSampleInstance(targetDomain);
             avgObjectiveScore = sum(foldObjectiveScores)/ numCVFold;
             avgTime = toc(TotalTimer)/ numCVFold;
-
+            
             if avgObjectiveScore < bestObjectiveScore
                 bestObjectiveScore = avgObjectiveScore;
                 bestAccuracy = accuracy*100;
                 bestTime = avgTime;
             end
             time = round(clock);
-%             fprintf('Time: %d/%d/%d,%d:%d:%d\n', time(1), time(2), time(3), time(4), time(5), time(6));
-%             fprintf('Initial try: %d, ObjectiveScore:%f, Accuracy:%f%%\n', t, avgObjectiveScore, accuracy*100);
+            %             fprintf('Time: %d/%d/%d,%d:%d:%d\n', time(1), time(2), time(3), time(4), time(5), time(6));
+            %             fprintf('Initial try: %d, ObjectiveScore:%f, Accuracy:%f%%\n', t, avgObjectiveScore, accuracy*100);
         end
     end
 end
