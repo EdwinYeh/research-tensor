818ad4f72b056072a8fb8efd7f2ab44cf246e02d
EdwinYeh <edwin02621@gmail.com>
2016-04-21 19:27:05 +0800

    (1) Ready for DY experiment (large sample)

--- Exp_DY_onenorm.m
+++ Exp_DY_onenorm.m
@@ -3,18 +3,18 @@ isTestPhase = true;
 exp_title = sprintf('DY_onenorm_%d', datasetId);
 resultFile = fopen(sprintf('../exp_result/%s.csv', exp_title), 'a');
 fprintf(resultFile, 'sigma,sigma2,lambda,delta,objectiveScore,accuracy,trainingTime\n');
-lambdaMaxOrder = 0;
-deltaMaxOrder = 0;
-randomTryTime = 5;
+lambdaMaxOrder = 6;
+deltaMaxOrder = 6;
+randomTryTime = 1;
 sigmaList = 0.05:0.05:0.05;
 sigma2 = -1;
 for tuneSigma = 1:length(sigmaList)
     sigma = sigmaList(tuneSigma);
     PrepareExperiment;
     for lambdaOrder = 0:lambdaMaxOrder
-        lambda = 10^-4 * 100 ^ lambdaOrder;
+        lambda = 10^-12 * 100 ^ lambdaOrder;
         for deltaOrder = 0:deltaMaxOrder
-            delta = 10^-4 * 10 ^ deltaOrder;
+            delta = 10^-12 * 100 ^ deltaOrder;
             main_DY_onenorm;
         end
         delta = 0;
