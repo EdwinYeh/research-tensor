cb30a871b7eef791812ddfaa75d8cdd5e1e1015f
EdwinYeh <edwin02621@gmail.com>
2016-05-10 16:42:47 +0800

    (1) Add parameter comment in Exp_DY

--- Exp_DY.m
+++ Exp_DY.m
@@ -1,3 +1,69 @@
+% dataset 1 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.015	0.0001	1.00E-13
+% 10	10	10	0.015	0.0001	1.00E-10
+% 10	10	10	0.005	0.004	1.00E-07
+% 10	10	10	0.015	0.0001	1.00E-16
+%  dataset 2 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.005	0.0001	1.00E-13
+% 10	10	10	0.005	0.004	1.00E-13
+% 10	10	10	0.005	0.16	1.00E-07
+% 10	10	10	0.005	0.0001	1.00E-07
+% dataset 3 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.005	0.0001	1.00E-13
+% 10	10	10	0.005	0.004	1.00E-16
+% 10	10	10	0.015	0.004	1.00E-10
+% 10	10	10	0.015	0.16	1.00E-13
+% dataset 4 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.025	0.004	1.00E-07
+% 10	10	10	0.015	0.0001	1.00E-10
+% 10	10	10	0.015	0.004	1.00E-16
+% 10	10	10	0.015	0.16	1.00E-07
+% dataset 5 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.005	0.16	1.00E-13
+% 10	10	10	0.015	0.004	1.00E-10
+% 10	10	10	0.015	0.004	1.00E-07
+% 10	10	10	0.005	6.4	1.00E-13
+% dataset 6 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.005	0.0001	1.00E-07
+% 10	10	10	0.015	6.4	1.00E-07
+% 10	10	10	0.005	0.16	1.00E-16
+% 10	10	10	0.005	0.004	1.00E-07
+% dataset 7 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.035	0.0001	1.00E-07
+% 10	10	10	0.035	0.004	1.00E-16
+% 10	10	10	0.035	0.004	1.00E-10
+% 10	10	10	0.035	0.0001	1.00E-10
+% dataset 8 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.035	0.0001	1.00E-16
+% 10	10	10	0.035	0.004	1.00E-13
+% 10	10	10	0.025	0.0001	1.00E-16
+% 10	10	10	0.025	0.004	1.00E-10
+% dataset 9 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.035	6.4	1.00E-13
+% 10	10	10	0.015	0.16	1.00E-16
+% 10	10	10	0.035	0.0001	1.00E-10
+% 10	10	10	0.005	0.0001	1.00E-13
+% dataset 10 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.015	0.16	1.00E-16
+% 10	10	10	0.015	0.004	1.00E-10
+% 10	10	10	0.025	0.16	1.00E-16
+% 10	10	10	0.015	0.16	1.00E-10
+% dataset 11 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.015	0.004	1.00E-16
+% 10	10	10	0.025	0.0001	1.00E-10
+% 10	10	10	0.015	0.0001	1.00E-07
+% 10	10	10	0.005	0.16	1.00E-13
+% dataset 12 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.025	0.004	1.00E-07
+% 10	10	10	0.025	6.4	1.00E-07
+% 10	10	10	0.025	0.004	1.00E-13
+% 10	10	10	0.035	6.4	1.00E-13
+% dataset 13 (cpRank,instanceCluster,featureCluster,sigma,lambda,delta)
+% 10	10	10	0.035	0.004	1.00E-13
+% 10	10	10	0.035	0.16	1.00E-16
+% 10	10	10	0.025	6.4	1.00E-13
+% 10	10	10	0.025	0.0001	1.00E-07
+
 SetParameter;
 resultDirectory = sprintf('../exp_result/DY/%d/', datasetId);
 mkdir(resultDirectory);
