835074c64431645ad2e1c4022946755b32ca5418
EdwinYeh <edwin02621@gmail.com>
2016-05-10 12:04:39 +0800

    Rewrite script of GCMF

--- baseline/GCMF/run_multi_GCMF.sh
+++ baseline/GCMF/run_multi_GCMF.sh
@@ -3,5 +3,5 @@ Idx_dataset="1 2 3 4 10"
 
 for idx in $Idx_dataset
 do
-  usr/local/MATLAB/R2012a/bin/matlab -r "datasetId = $idx; Exp_GCMF; exit;
+  usr/local/MATLAB/R2012a/bin/matlab -r "datasetId = $idx; Exp_GCMF; exit;"
 done
