44902b6083fb3746dba8198e0ca22ddf6ce37276
EdwinYeh <edwin02621@gmail.com>
2016-08-18 02:07:20 +0800

    (1) Fix bug of creating different combination of userId

--- clustering/runUserCombinationJob.m
+++ clustering/runUserCombinationJob.m
@@ -1,5 +1,6 @@
 function runUserCombinationJob( minUserId, maxUserId, minJobId, maxJobId )
-    jobCombination = combinator(maxUserId,minUserId,'c');
+    jobCombination = combinator(maxUserId-minUserId+1, 2,'c');
+    jobCombination = jobCombination - (1-minUserId);
     for jobId = minJobId:maxJobId
         fprintf('Run job:[%d,%d]\n', jobCombination(jobId,1), jobCombination(jobId,2));
         Exp_mturk(jobCombination(jobId,:));
