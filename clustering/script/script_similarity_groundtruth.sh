31b05c12e485c5c5c924b1466c88a4b890d7f781
EdwinYeh <edwin02621@gmail.com>
2016-08-30 15:11:09 +0800

    (1) Edit script of clustering

--- clustering/script/script_similarity_groundtruth.sh
+++ clustering/script/script_similarity_groundtruth.sh
@@ -2,5 +2,5 @@ UserIdList="75 79 85 90 65"
 
 for userId in $UserIdList
 do
-	/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0, userId]);"
+	/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0, $userId]);" &
 done
