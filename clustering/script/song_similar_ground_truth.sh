1d16bd018de0c9f102df388231b84868985d0be3
EdwinYeh <edwin02621@gmail.com>
2016-09-07 01:25:09 +0800

    (1) Fix scipt of song dataset

--- clustering/script/song_similar_ground_truth.sh
+++ clustering/script/song_similar_ground_truth.sh
@@ -2,5 +2,5 @@ UserIdList="3 5 7 6 9"
 
 for userId in $UserIdList
 do
- /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [0, $userId])" &
+ /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0, $userId])" &
 done
