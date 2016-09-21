ca06c251553e547b13afacd2db34c33bb62e10e0
EdwinYeh <edwin02621@gmail.com>
2016-09-21 15:42:35 +0800

    (1) Add script of experiment of perception seed rate on song & citation

---
+++ clustering/script/citation_perception_seed_rate.sh
@@ -0,0 +1,11 @@
+PerceptionSeedRateList="0.1 0.3 0.5 0.7 0.9"
+
+for perceptionSeedRate in $PerceptionSeedRateList
+do
+ /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21, 154], $perceptionSeedRate)" &
+done
+
+for perceptionSeedRate in $PerceptionSeedRateList
+do
+ /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21, 154, 131], $perceptionSeedRate)" &
+done
