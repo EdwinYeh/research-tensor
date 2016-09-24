57d4fe03206099c59369c5953661292ec03f665e
EdwinYeh <edwin02621@gmail.com>
2016-09-24 14:01:01 +0800

    (1) Rewrite script of citation

--- clustering/script/citation_perception_seed_rate.sh
+++ clustering/script/citation_perception_seed_rate.sh
@@ -1,11 +1,11 @@
 PerceptionSeedRateList="0.1 0.3 0.5"
 
-for perceptionSeedRate in $ PerceptionSeedRateList
+for perceptionSeedRate in $PerceptionSeedRateList
 do
     /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21, 154], $perceptionSeedRate)" &
 done
 
-for perceptionSeedRate in $ PerceptionSeedRateList
+for perceptionSeedRate in $PerceptionSeedRateList
 do
     /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21, 154, 131], $perceptionSeedRate)" &
 done
