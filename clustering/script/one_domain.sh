31c3825e1c268327e5f82f5af718e277244fb02a
EdwinYeh <edwin02621@gmail.com>
2016-10-13 00:55:56 +0800

    (1) Edit script of one domain exp

--- clustering/script/one_domain.sh
+++ clustering/script/one_domain.sh
@@ -1,3 +1,11 @@
 /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0], 0.5, -1);"&
 /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0], 0.5, -1);"&
 /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21], 0.5, -1);"&
+
+/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0], 0.3, -1);"&
+/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0], 0.3, -1);"&
+/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21], 0.3, -1);"&
+
+/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0], 0.1, -1);"&
+/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0], 0.1, -1);"&
+/usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21], 0.1, -1);"&
