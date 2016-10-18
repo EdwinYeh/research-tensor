47c1cceb18507a790a2230e010d7a1c19facd189
EdwinYeh <edwin02621@gmail.com>
2016-10-18 18:55:17 +0800

    (1) Rewrite script

--- clustering/script/cluster_seed_rate.sh
+++ clustering/script/cluster_seed_rate.sh
@@ -1,6 +1,7 @@
-ClusterSeedRateList="0.3 0.6 0.9"
+ClusterSeedRateList="0.25 0.5 0.75"
 
 for clusterSeedRate in $ClusterSeedRateList
 do
   /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0, 64, 74], 0.5, $clusterSeedRate);" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0, 3, 5], 0.5, $clusterSeedRate);" &
 done
