d0efb3274442d3e2e4c3b82f8ee79ac654fa4a59
EdwinYeh <edwin02621@gmail.com>
2016-10-18 19:15:34 +0800

    (1) Edit scipt

--- clustering/script/perception_seed_rate.sh
+++ clustering/script/perception_seed_rate.sh
@@ -3,9 +3,11 @@ PerceptionSeedRateList="0.5"
 for perceptionSeedRate in $PerceptionSeedRateList
 do
  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0, 64], $perceptionSeedRate, 1)" &
+ /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0, 3], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0, 64, 74], $perceptionSeedRate, 1)" &
+ /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0, 3, 5], $perceptionSeedRate, 1)" &
 done
