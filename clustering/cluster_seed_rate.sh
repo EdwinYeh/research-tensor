a583bc105bd3b53e228aad4b14e3a2ca0b12efdd
EdwinYeh <edwin02621@gmail.com>
2016-10-20 21:48:54 +0800

    (1) Add new script of zero-shot exp for song dataset

--- clustering/cluster_seed_rate.sh
+++
@@ -1,7 +0,0 @@
-ClusterSeedRateList="0.25 0.5 0.75"
-
-for clusterSeedRate in $ClusterSeedRateList
-do
- /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('mturk', [0, 64, 74], ,0.6 ,$clusterSeedRate)" &
- /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('song', [0, 3, 5], ,0.6 ,$clusterSeedRate)" &
-done
--- clustering/runUserCombinationJob.m
+++
@@ -1,9 +0,0 @@
-function runUserCombinationJob( minUserId, maxUserId, minJobId, maxJobId )
-    jobCombination = combinator(maxUserId-minUserId+1, 2,'c');
-    jobCombination = jobCombination - (1-minUserId);
-    for jobId = minJobId:maxJobId
-        fprintf('Run job:[%d,%d]\n', jobCombination(jobId,1), jobCombination(jobId,2));
-        Exp_mturk(jobCombination(jobId,:));
-    end
-end
-
