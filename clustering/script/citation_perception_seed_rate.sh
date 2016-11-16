6320c7b27d3971b0df91d563d0b5e8d0a91cc0c6
EdwinYeh <edwin02621@gmail.com>
2016-11-16 20:48:03 +0800

    (1) Rewrite script of citation dataset (2) Generate seedset for new user

--- clustering/script/citation_perception_seed_rate.sh
+++ clustering/script/citation_perception_seed_rate.sh
@@ -1,16 +1,16 @@
-PerceptionSeedRateList="0.5 0.7"
+PerceptionSeedRateList="0.3 0.5 0.7"
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [22], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [22], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [22,83], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [22,27], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [22,83,144], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [22,27,142], $perceptionSeedRate, 1)" &
 done
