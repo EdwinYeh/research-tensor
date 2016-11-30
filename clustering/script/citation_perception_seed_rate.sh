b0d64ededb9a9c099a9de87ceee5151e42a4fd74
EdwinYeh <edwin02621@gmail.com>
2016-11-30 12:16:39 +0800

    (1) Rewrite script of citation to run perception seed rate = 0.7 (2) Rollback the chosen userId for citation dataset

--- clustering/script/citation_perception_seed_rate.sh
+++ clustering/script/citation_perception_seed_rate.sh
@@ -1,16 +1,16 @@
-PerceptionSeedRateList="0.3 0.5 0.7"
+PerceptionSeedRateList="0.7"
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [22], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [21], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [22,27], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [21,154], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [22,27,142], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [21,154,131], $perceptionSeedRate, 1)" &
 done
