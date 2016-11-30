1380828fdb8281e0f7ca550400b1b50f53ab5eee
EdwinYeh <edwin02621@gmail.com>
2016-11-30 12:21:00 +0800

    (1) Correct script of citation

--- clustering/script/citation_perception_seed_rate.sh
+++ clustering/script/citation_perception_seed_rate.sh
@@ -2,15 +2,15 @@ PerceptionSeedRateList="0.7"
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [21], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [21,154], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21,154], $perceptionSeedRate, 1)" &
 done
 
 for perceptionSeedRate in $PerceptionSeedRateList
 do
-  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation2', [21,154,131], $perceptionSeedRate, 1)" &
+  /usr/local/MATLAB/R2012a/bin/matlab -r "cd ..; Exp_clustering('citation', [21,154,131], $perceptionSeedRate, 1)" &
 done
